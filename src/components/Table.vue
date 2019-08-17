9719c85c206bde7e82ecdf3f3f353fb2ca003ab2
Leandro <leaxviana140@gmail.com>
2019-08-17 00:34:57 -0400

    table and background

---
+++ src/components/Table.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="table">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .table{
+    height: 600px;
+    width: 600px;
+    background-color: #000;
+    margin: 0 auto;
+  background-image: linear-gradient(to bottom, #503c3a, #5d4642, #6a5149, #775c51, #846758);
+  }
+</style>
