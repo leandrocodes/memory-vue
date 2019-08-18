f960fa959f4f469720880ebb6bce46aaa3a52c08
Leandro <leaxviana140@gmail.com>
2019-08-18 11:39:30 -0400

    new cards array

--- src/cards.js
+++ src/cards.js
@@ -1,14 +1,38 @@
 export default[
-    {name:'lupa',img:'https://image.flaticon.com/icons/svg/1150/1150626.svg', flipped: false},
-    {name: 'comp',img: 'https://image.flaticon.com/icons/svg/1149/1149168.svg', flipped: false},
-    {name: 'config',img: 'https://image.flaticon.com/icons/svg/148/148909.svg', flipped: false},
-    {name: 'lupa',img: 'https://image.flaticon.com/icons/svg/1150/1150626.svg', flipped: false},
-    {name: 'comp',img: 'https://image.flaticon.com/icons/svg/1149/1149168.svg', flipped: false},
-    {name: 'config',img: 'https://image.flaticon.com/icons/svg/148/148909.svg', flipped: false},
-    {name: 'idea',img: 'https://image.flaticon.com/icons/svg/427/427735.svg', flipped: false},
-    {name: 'brain',img: 'https://image.flaticon.com/icons/svg/791/791072.svg', flipped: false},
-    {name: 'pencils', img: 'https://image.flaticon.com/icons/svg/1497/1497726.svg', flipped: false},
-    {name: 'idea',img: 'https://image.flaticon.com/icons/svg/427/427735.svg', flipped: false},
-    {name: 'brain',img: 'https://image.flaticon.com/icons/svg/791/791072.svg', flipped: false},
-    {name: 'pencils',img: 'https://image.flaticon.com/icons/svg/1497/1497726.svg', flipped: false}
+    
+    {name:'lupa',img:'https://image.flaticon.com/icons/svg/1150/1150626.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'comp',img: 'https://image.flaticon.com/icons/svg/1149/1149168.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'config',img: 'https://image.flaticon.com/icons/svg/148/148909.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'lupa',img: 'https://image.flaticon.com/icons/svg/1150/1150626.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'comp',img: 'https://image.flaticon.com/icons/svg/1149/1149168.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'config',img: 'https://image.flaticon.com/icons/svg/148/148909.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'idea',img: 'https://image.flaticon.com/icons/svg/427/427735.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'brain',img: 'https://image.flaticon.com/icons/svg/791/791072.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'pencils', img: 'https://image.flaticon.com/icons/svg/1497/1497726.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'idea',img: 'https://image.flaticon.com/icons/svg/427/427735.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'brain',img: 'https://image.flaticon.com/icons/svg/791/791072.svg', 
+        flipped: false, wrongChoose: false, discovered: false},
+    
+    {name: 'pencils',img: 'https://image.flaticon.com/icons/svg/1497/1497726.svg', 
+        flipped: false, wrongChoose: false, discovered: false}
 ]
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -16,6 +16,9 @@ export default {
 		return {
 			cards
 		}
+	},
+	computed:{
+
 	}
 }
 </script>
